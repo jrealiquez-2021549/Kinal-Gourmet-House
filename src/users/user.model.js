fc5eed0584e146ea13f3ab881a0ec8dc51144a9d
aalvarez-2024004 <aalvarez-2024004@kinal.edu.gt>
2026-02-11 22:41:41 -0600

    Se uso bycrypt para poder encriptar las contrasenas y que asi, no queden en un texto plano

--- src/users/user.model.js
+++ src/users/user.model.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
 
 const userSchema = mongoose.Schema(
     {
@@ -54,5 +56,15 @@ const userSchema = mongoose.Schema(
 
 userSchema.index({ isActive: 1 });
 userSchema.index({ role: 1 });
+
+//Antes de guardar un usuario se ejecuta esta funcion
+//Si la contra no fue modificada, entonces no se encripta otra vez
+// Esta funcion reemplaza la contrasena original y la encripta por medio de bycrypt
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
 export default mongoose.model('User', userSchema);
