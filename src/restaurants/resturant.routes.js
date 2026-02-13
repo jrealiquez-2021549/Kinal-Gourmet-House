e76bb6ffa944c894de3bc8bc47c79cb566a01755
alacan-2024010 <alacan-2024010@kinal.edu.gt>
2026-02-13 15:03:32 -0600

    Se agregaron las rutas para que se pueda subir la imagen

--- src/restaurants/resturant.routes.js
+++ src/restaurants/resturant.routes.js
@@ -5,11 +5,21 @@ import {
     //updateRestaurant,
     //deleteRestaurant
 } from "./restaurant.controller.js";
+import{createCloudinar}from '../../middlewares/files-uploaders.js';
+import{cleanUploaderFile} from '../../middlewares/delete-files-on-error.js';
 
 const router = Router();
 
-router.post('/create', createRestaurant);
-router.get('/', getRestaurants);
+router.post(
+    '/create',
+    createCloudinar.array('images', 3),
+    cleanUploaderFile,
+    createRestaurant
+);
+router.get(
+    '/', 
+    getRestaurants
+);
 //router.put('/update/:id', updateRestaurant);
 //router.delete('/delete/:id', deleteRestaurant);
 
--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { createUser, getUsers } from "./user.controller.js";
+import {cleanUploaderFile} from '../../middlewares/delete-files-on-error.js'
+import {createCloudinary} from '../../middlewares/files-uploaders.js'
 
 const router = Router();
 
 router.post(
     '/create',
+    createCloudinary.single('profileImage'),
+    cleanUploaderFile,
     createUser
 )
 
